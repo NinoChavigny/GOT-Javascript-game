2b119ab81ce3ed2c874d60c8ec7914cbf38772aa
Nino <nino.chavigny@outlook.fr>
2023-01-25 17:51:26 +0100

    Last version 25/01

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
+
+//load assets static
+app.use('/static',express.static(path.join(__dirname, 'public')))
+app.use('/assets',express.static(path.join(__dirname, 'public/assets')))
 
 //home route
 app.get('/',(req,res)=>{
