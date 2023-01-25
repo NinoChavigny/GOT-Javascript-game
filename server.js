08a230f7ae4f7f075a7c60650abffc7c6a8ad450
Nino <nino.chavigny@outlook.fr>
2023-01-25 17:51:45 +0100

    Last Version 25/01

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
+
 const path = require('path');
+const bodyparser = require("body-parser");
+const session = require("express-session");
+const {v4: uuidv4 } = require("uuid");
 
 const port = process.env.PORT || 3000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.set('view engine','ejs');
 
 //load assets static
 app.use('/static',express.static(path.join(__dirname, 'public')))
 app.use('/assets',express.static(path.join(__dirname, 'public/assets')))
+
+app.use(session({
+    secret: 'uuidv4()',
+    resave: false,
+    saveUninitialized: true
+}))
 
 //home route
 app.get('/',(req,res)=>{
